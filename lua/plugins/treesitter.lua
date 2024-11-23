a88f416d35f0bdead16d251f0ddd8b0ad3bb75c7
SASOPELANA <sopekof@gmail.com>
2024-11-22 22:02:44 -0300

    lsp nativo

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,44 @@
+vim.cmd([[highlight TSCurrentScope guibg=#75f]])
+
+return {
+  "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-refactor",
+  },
+  main = "nvim-treesitter.configs",
+  opts = {
+    ensure_installed = {
+      "html",
+      "css",
+      "scss",
+      "vue",
+      "javascript",
+      "typescript",
+      "tsx",
+      "svelte",
+      "json",
+      "go",
+      "python",
+      "cpp",
+      "c_sharp",
+      "angular",
+    },
+    highlight = {
+      enable = true,
+      use_languagetree = true,
+    },
+    refactor = {
+      highlight_definitions = {
+        enable = true,
+        clear_on_cursor_move = true,
+      },
+      highlight_current_scope = { enable = true },
+      smart_rename = {
+        enable = true,
+        keymaps = {
+          smart_rename = "tr",
+        },
+      },
+    },
+  },
+}

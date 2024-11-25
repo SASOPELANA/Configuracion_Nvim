f3e986538c8e9b4d2bb1dd95fa1d52a8a24abc5a
Sergio <sopekof@gmail.com>
2024-11-24 23:34:37 -0300

    Nvim - Configuracion

--- lua/plugins/toggleterm_terminal.lua
+++ lua/plugins/toggleterm_terminal.lua
@@ -9,7 +9,7 @@ return {
       direction = "horizontal", -- Dirección del terminal ('horizontal', 'vertical', 'tab')
       shade_terminals = true, -- Habilitar sombreado en el terminal
       start_in_insert = true, -- Iniciar en modo de inserción
-      shell = "pwsh", -- Usar PowerShell Core
+      shell = "/usr/bin/zsh", -- Usar PowerShell Core
       close_on_exit = true, -- Cerrar el terminal cuando el proceso finalice
     },
   },

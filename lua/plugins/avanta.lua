927748942cf22407ab6fbe2f011cfb6148f0fbfa
Sergio <sopekof@gmail.com>
2025-01-08 14:05:50 -0300

    Nvim - Avanta

---
+++ lua/plugins/avanta.lua
@@ -0,0 +1,54 @@
+return {
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  lazy = false,
+  version = false, -- set this to "*" if you want to always pull the latest change, false to update on release
+  opts = {
+    provider = "copilot", -- Usa Copilot como proveedor predeterminado
+    auto_suggestions_provider = "copilot", -- Copilot para sugerencias automáticas
+  },
+  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  build = "make",
+  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+  dependencies = {
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    --- The below dependencies are optional,
+    "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    {
+      "github/copilot.vim", -- Copilot oficial de GitHub
+      config = function()
+        -- Configuración opcional para copilot.vim
+        vim.g.copilot_no_tab_map = true -- Deshabilitar el mapeo por defecto de <Tab>
+        vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+      end,
+    },
+    {
+      -- support for image pasting
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          -- required for Windows users
+          use_absolute_path = true,
+        },
+      },
+    },
+    {
+      -- Make sure to set this up properly if you have lazy=true
+      "MeanderingProgrammer/render-markdown.nvim",
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
+  },
+}

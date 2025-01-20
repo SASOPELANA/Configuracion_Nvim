e37f762299c7053a8deb21855de788e9b4f2350a
Sergio <sopekof@gmail.com>
2025-01-20 18:14:44 -0300

    github themes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -73,7 +73,7 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme("kanagawa") -- habilitar por defecto
+      -- vim.cmd.colorscheme("kanagawa") -- habilitar por defecto
     end,
   },
 
@@ -111,6 +111,21 @@ return {
       -- vim.cmd.colorscheme("synthweave")
       -- transparent version
       -- vim.cmd.colorscheme("synthweave-transparent")
+    end,
+  },
+
+  -- GitHub Themes
+  {
+    "projekt0n/github-nvim-theme",
+    name = "github-theme",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("github-theme").setup({
+        -- ...
+      })
+
+      vim.cmd("colorscheme github_dark")
     end,
   },
 }

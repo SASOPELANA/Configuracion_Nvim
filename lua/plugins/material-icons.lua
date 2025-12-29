004fc7d1f81feb7ec2d25ad0d45f8b46f7be6811
Sergio <sopekof@gmail.com>
2025-12-29 16:49:34 -0300

    icons

--- lua/plugins/material-icons.lua
+++ lua/plugins/material-icons.lua
@@ -12,6 +12,7 @@ return {
   },
   {
     "DaikyXendo/nvim-material-icon",
+    enabled = true,
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
       -- Configura el icono personalizado para archivos .css
@@ -22,7 +23,31 @@ return {
             color = "#663399", -- Color azul (ajústalo si quieres)
             name = "Css",
           },
+          ["gitignore"] = {
+            icon = "",
+            color = "#F1502F",
+            name = "Gitignore",
+          },
+          ["prettierrc"] = {
+            icon = "p",
+            color = "#F7A944",
+            name = "Prettier",
+          },
+          ["env"] = {
+            icon = "",
+            color = "#F7A944",
+            name = "Env",
+          },
         },
+
+        icon_by_filename = {
+          ["Dockerfile"] = {
+            icon = "",
+            color = "#E5F2FC",
+            name = "Dockerfile",
+          },
+        },
+
         default = false,
         color_icons = true,
       })

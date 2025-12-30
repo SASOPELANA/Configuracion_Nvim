6c13fbfaa9d604d88aa0a213c78f46e7b18cf1a2
Sergio <sopekof@gmail.com>
2025-12-30 01:12:43 -0300

    icon - file - folders

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -8,5 +8,10 @@ vim.api.nvim_create_autocmd("ColorScheme", {
     vim.api.nvim_set_hl(0, "MiniIconsMdx", { fg = "#519ABA" })
     vim.api.nvim_set_hl(0, "MiniIconsTs", { fg = "#0188d1" })
     vim.api.nvim_set_hl(0, "MiniIconsSh", { fg = "#ff7043" })
+    vim.api.nvim_set_hl(0, "MiniIconsMiddleware", { fg = "#6a5acd" })
+    vim.api.nvim_set_hl(0, "MiniIconsComponent", { fg = "#a98f43" })
+    vim.api.nvim_set_hl(0, "MiniIconsApi", { fg = "#b4c555" })
+    vim.api.nvim_set_hl(0, "MiniIconsContext", { fg = "#2bb153" })
+    vim.api.nvim_set_hl(0, "MiniIconsConfig", { fg = "#1bd1c2" })
   end,
 })
---
+++ lua/plugins/icons-config.lua
@@ -0,0 +1,82 @@
+return {
+  {
+    "nvim-mini/mini.icons",
+    lazy = true,
+    opts = {
+      -- Configuración principal de iconos
+      default = {
+        -- Ejemplo: puedes sobreescribir el icono por defecto para archivos
+        file = { glyph = "󰈙" },
+      },
+      -- AQUÍ: Personalizar iconos por EXTENSIÓN (no por 'filetype')
+      extension = {
+        mdx = { glyph = " ", hl = "MiniIconsMdx" },
+        css = { glyph = " ", hl = "MiniIconsCss" },
+        md = { glyph = " ", hl = "MiniIconsGrey" },
+        ts = { glyph = "󰛦 ", hl = "MiniIconsTs" },
+        env = { glyph = " ", hl = "MiniIconsYellow" },
+        http = { glyph = "󰖟 ", hl = "MiniIconsRed" },
+        sh = { glyph = "󰆍 ", hl = "MiniIconsSh" },
+      },
+      -- Puedes seguir configurando otras categorías si lo necesitas:
+      filetype = {
+        -- Aquí irían personalizaciones para tipos como 'lua', 'python', etc.
+      },
+      file = {
+        [".keep"] = { glyph = "󰊢", hl = "MiniIconsOrange" },
+        ["devcontainer.json"] = { glyph = "", hl = "MiniIconsAzure" },
+        [".env"] = { glyph = " ", hl = "MiniIconsYellow" },
+      },
+      directory = {
+        ["controllers"] = { glyph = "󱁿 ", hl = "MiniIconsYellow" },
+        ["controller"] = { glyph = "󱁿 ", hl = "MiniIconsYellow" },
+        ["middleware"] = { glyph = "󱧼 ", hl = "MiniIconsMiddleware" },
+        ["middlewares"] = { glyph = "󱧼 ", hl = "MiniIconsMiddleware" },
+        ["routes"] = { glyph = "󱧬 ", hl = "MiniIconsGreen" },
+        ["route"] = { glyph = "󱧬 ", hl = "MiniIconsGreen" },
+        ["private"] = { glyph = "󰉐 ", hl = "MiniIconsRed" },
+        ["privates"] = { glyph = "󰉐 ", hl = "MiniIconsRed" },
+        ["models"] = { glyph = "󱋣 ", hl = "MiniIconsSh" },
+        ["model"] = { glyph = "󱋣 ", hl = "MiniIconsSh" },
+        ["types"] = { glyph = "󱧶 ", hl = "MiniIconsTs" },
+        ["type"] = { glyph = "󱧶 ", hl = "MiniIconsTs" },
+        ["validators"] = { glyph = "󱥾 ", hl = "MiniIconsRed" },
+        ["validator"] = { glyph = "󱥾 ", hl = "MiniIconsRed" },
+        ["components"] = { glyph = "󰉎 ", hl = "MiniIconsComponent" },
+        ["component"] = { glyph = "󰉎 ", hl = "MiniIconsComponent" },
+        ["pages"] = { glyph = "󰾷 ", hl = "MiniIconsOrange" },
+        ["page"] = { glyph = "󰾷 ", hl = "MiniIconsOrange" },
+        ["styles"] = { glyph = "󱃪 ", hl = "MiniIconsCss" },
+        ["style"] = { glyph = "󱃪 ", hl = "MiniIconsCss" },
+        ["apis"] = { glyph = "󰴉 ", hl = "MiniIconsApi" },
+        ["api"] = { glyph = "󰴉 ", hl = "MiniIconsApi" },
+        ["contexts"] = { glyph = " ", hl = "MiniIconsContext" },
+        ["context"] = { glyph = " ", hl = "MiniIconsContext" },
+        ["assets"] = { glyph = "󰲂 ", hl = "MiniIconsYellow" },
+        ["asset"] = { glyph = "󰲂 ", hl = "MiniIconsYellow" },
+        ["layouts"] = { glyph = " ", hl = "MiniIconsCyan" },
+        ["layout"] = { glyph = " ", hl = "MiniIconsCyan" },
+        ["config"] = { glyph = "󱁿 ", hl = "MiniIconsConfig" },
+      },
+
+      -- Configuración opcional: función para controlar extensiones
+      use_file_extension = function(ext, file)
+        -- Ejemplo: ignorar extensión 'log' para usar detección de filetype
+        if ext == "log" then
+          return false
+        end
+        return true
+      end,
+    },
+    init = function()
+      -- MAGIA: Hace que 'mini.icons' controle las llamadas a 'nvim-web-devicons'
+      package.preload["nvim-web-devicons"] = function()
+        require("mini.icons").mock_nvim_web_devicons()
+        return package.loaded["nvim-web-devicons"]
+      end
+    end,
+    config = function(_, opts)
+      require("mini.icons").setup(opts)
+    end,
+  },
+}
